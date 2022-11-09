51715cb0df4ddc4732f9e7d718441e8630032492
ArthurGit-72 <82040178+ArthurGit-72@users.noreply.github.com>
2022-11-09 23:49:11 +0300

    Add files via upload
    Final version

--- script.js
+++ script.js
@@ -3,4 +3,15 @@ $(document).ready(function() {
         $('.header__burger,.header__menu').toggleClass('active');
         $('body').toggleClass('lock')
     });
+    $('.block__title').click(function(event) {
+        // if($('.block').hasClass('one')){
+        //    $('.block__title').not($(this)).removeClass('active');
+        //    $('.block__text').not($(this).next()).slideUp(300);
+
+        // }
+        // $(this).toggleClass('active').next().slideToggle(300);
+
+        $(this).parent('.footer__pages').toggleClass('footer__pages_active');
+
+    });
 });
